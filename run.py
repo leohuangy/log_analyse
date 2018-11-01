1250c30e32954bfb99f0117cb373bf2e78ff593b
huangyang <huangyang@wifi.com>
2018-11-01 19:24:43 +0800

    xiugai

--- run.py
+++ run.py
@@ -8,31 +8,29 @@
 @license: personal
 @file: run.py
 @time: 2018-10-03
+@desc 如需置顶testcase目录，则直接python run.py 11即可，11为testcase下的目录名称，默认情况直接python run.py即可跑所有testcase下面的用例
 """
 
 import unittest
-import HTMLTestRunner
 import datetime
-from config import globalparam
+import sys
 
 
-def run():
+def run(test_case):
     print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " 开始执行 \n"
-
     test_dir = './testcase'
-    suite = unittest.defaultTestLoader.discover(start_dir=test_dir, pattern='test*.py')
+    if test_case != 'all':
+        test_dir += '/' + test_case
 
-    reportname = globalparam.report_path + '\\' + 'TestResult' + now + '.html'
-    with open(reportname, 'wb') as f:
-        runner = HTMLTestRunner.HTMLTestRunner(
-            stream=f,
-            title='测试报告',
-            description='Test the import testcase'
-        )
-        runner.run(suite)
+    print test_dir
+    suite = unittest.defaultTestLoader.discover(start_dir=test_dir, pattern='test*.py')
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
 
     print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " 结束执行 \n"
 
 
 if __name__ == '__main__':
-    run()
+    # print sys.argv
+    test_case = sys.argv[1] if len(sys.argv) > 1 else 'all'
+    run(test_case)
